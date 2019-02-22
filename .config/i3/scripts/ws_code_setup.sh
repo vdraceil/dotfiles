b66bf141466f2f4adfd5d191fd236306bc0605b7
Vinoth Kumar Kannan <vdraceil@gmail.com>
2019-02-22 17:08:06 +0700

    i3wm - Adding code_layout mode and supporting scripts/layouts

---
+++ .config/i3/scripts/ws_code_setup.sh
@@ -0,0 +1,47 @@
+WS_DIR=$HOME/Workplace
+WS_PERSONAL_DIR=$WS_DIR/Personal
+WS_FREELANCE_DIR=$WS_DIR/Freelance
+
+PANES_COUNT=$1
+I3_WS_LAYOUT="$HOME/.config/i3/layouts/ws_code_${PANES_COUNT}panes.json"
+
+# compile projects list, recently modified first
+personalProjects=$(
+  find $WS_PERSONAL_DIR -mindepth 1 -maxdepth 1 -type d -printf '%T@\t%p\n' |
+  grep -vP 'env$' |
+  cut -f2 |
+  sed -n "s|${WS_PERSONAL_DIR}|\$WS_PERSONAL_DIR|p")
+freelanceProjects=$(
+  find $WS_FREELANCE_DIR -mindepth 2 -maxdepth 2 -type d -printf '%T@\t%p\n' |
+  grep -vP '(node_modules|__pycache__|output|_ROUGH|env$)' |
+  cut -f2 |
+  sed -n "s|${WS_FREELANCE_DIR}|\$WS_FREELANCE_DIR|p")
+projects=("\$WS_PERSONAL_DIR" "${personalProjects[@]}"
+  "\$WS_FREELANCE_DIR" "${freelanceProjects[@]}")
+
+# select project
+selectedProject=$(eval echo $(
+  printf "%s\n" "${projects[@]}" | rofi -dmenu -i -no-custom))
+
+# select workspace
+workspaces="5: code|6: code|7: code|8|9|10"
+selectedWorkspace=$(echo $workspaces | rofi -dmenu -sep '|' -i -no-custom)
+
+# set up workspace
+if [ -f "$I3_WS_LAYOUT" -a -n "$selectedProject" -a -n "$selectedWorkspace" ]
+then
+  # kill all windows in the selected workspace
+  i3-msg -q "[workspace=\"$selectedWorkspace\"] kill"
+
+  # push new layout
+  i3-msg -q "workspace \"$selectedWorkspace\"; append_layout $I3_WS_LAYOUT"
+
+  # start applications to be absorbed into the placeholders
+  (urxvt -cd $selectedProject -e vim &)
+  for (( i=1; i<$PANES_COUNT; i++ ))
+  do
+    (urxvt -cd $selectedProject &)
+  done
+else
+  exit 1
+fi
