b40175daa2a6898f02ecc950a2a4c9029588eef3
Vinoth Kumar Kannan <vdraceil@gmail.com>
2019-02-23 18:26:21 +0700

    Install Script - Add systemd services and scripts

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@
 sourceDir=$( cd "$( dirname "$0" )" && pwd )
 
 # make required directories
-mkdir $HOME/.config
+mkdir -p $HOME/.config
 
 # define the files/dirs for which we need symlinks in the $HOME dir
 sourceFiles=".bashrc .bash_profile .vimrc .gitconfig .gitignore .xinitrc
 .Xresources .ncmpcpp .config/polybar .config/ranger .config/mpd .config/i3
-.config/dunst .config/neofetch .config/htop .config/rofi"
+.config/dunst .config/neofetch .config/htop .config/rofi .config/systemd
+.config/scripts"
 
 # this function creates symlinks in $HOME dir
 # existing symlinks won"t be overridden unless the "--force" option is passed
@@ -20,7 +21,7 @@ function create_symlink() {
 
     echo -n "[SYMLINK] $dest -> $src ... "
     if [ -e $dest -a $force -eq 0 ]; then
-        echo "FAILURE! File already exists: $src"
+        echo "FAILED! Destination already exists"
     else
         if [ -d $dest -a $force -eq 1 ]; then
             rm -rf $dest
@@ -41,3 +42,9 @@ for file in $sourceFiles; do
 
     create_symlink $src $dest $force
 done
+
+# special symlink for systemctl service to show lockscreen on suspend
+if [ "$1" = "-s" -o "$1" = "--sudo" -o "$2" = "-s" -o "$2" = "--sudo" ]; then
+  sudo ln -sf "$HOME/.config/systemd/etc/suspend@vdraceil.service" \
+    "/etc/systemd/system/suspend@vdraceil.service"
+fi
