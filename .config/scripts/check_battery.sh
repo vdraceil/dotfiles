2eb7c134a064fbb6efff5d66bf1b8a6f0dbce6ff
Vinoth Kumar Kannan <vdraceil@gmail.com>
2019-02-24 01:26:22 +0700

    Systemd - User Script - Using proper integer comparision

--- .config/scripts/check_battery.sh
+++ .config/scripts/check_battery.sh
@@ -3,7 +3,7 @@
 CRITICAL_BATTERY_PERCENT=10
 
 BATTERY_PERCENT=$(acpi -b | grep -oP '(?<=Discharging, )\d+(?=%)')
-if [[ $BATTERY_PERCENT < $CRITICAL_BATTERY_PERCENT ]]
+if [ "$BATTERY_PERCENT" -lt "$CRITICAL_BATTERY_PERCENT" ]
 then
     DISPLAY=:0 notify-send -u critical \
       "Battery Low" "Remaining: ${BATTERY_PERCENT}%"
