7883aaf9e3a98c623c9474430a94e4d3160d068f
Vinoth Kumar Kannan <vdraceil@gmail.com>
2019-02-24 18:09:51 +0700

    Udiskie - Auto mount USB devices and optionally detach when required

---
+++ .config/scripts/usb_detach.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+mountedUSBMedia=$(mount | grep -oP '/run/media/[^ ]+')
+if [ -n "$mountedUSBMedia" ]
+then
+    selected=$(echo -e "$mountedUSBMedia" | rofi -dmenu -p \
+        "USB Device to Detach" -i -no-custom)
+    [ -n "$selected" ] && udiskie-umount --detach "$selected"
+    exit 0
+else
+  notify-send -u low "No Action" "No USB Device mounted"
+fi
