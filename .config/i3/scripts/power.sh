7e977e3df270c5b92d163d9a810708b63e91c7bd
Vinoth Kumar Kannan <vdraceil@gmail.com>
2019-02-26 17:14:27 +0700

    i3wm - Scripts - Show icons in power options and optimize rofi look

--- .config/i3/scripts/power.sh
+++ .config/i3/scripts/power.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
 
 # get user option
-options='reboot\nshutdown\nsuspend\nlogout'
-selected=$(echo -e $options | rofi -dmenu -no-custom)
+options=' reboot\n shutdown\n suspend\n logout'
+selected=$(echo -e $options | rofi -dmenu \
+  -no-custom -lines 4 -width 10 -line-padding 10)
 
 # execute
 case $selected in
-  "reboot") systemctl reboot;;
-  "shutdown") systemctl poweroff;;
-  "suspend") systemctl suspend;;
-  "logout") i3-msg exit;;
+  " reboot") systemctl reboot;;
+  " shutdown") systemctl poweroff;;
+  " suspend") systemctl suspend;;
+  " logout") i3-msg exit;;
 esac
