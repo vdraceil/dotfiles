b29f58a272485ff00155f7cb1fd2164f7ce93798
Vinoth Kumar Kannan <vdraceil@gmail.com>
2020-01-09 18:20:43 +0700

    i3wm - Removing unnecessary and resource intensive components from the dashboard

--- .config/i3/scripts/ws_dashboard.sh
+++ .config/i3/scripts/ws_dashboard.sh
@@ -12,9 +12,6 @@ i3-msg -q "workspace \"$WS1\"; append_layout $I3_WS_LAYOUT"
 # start applications to be absorbed into the placeholders
 (urxvt -e ranger "$HOME/Downloads" &)
 (urxvt -e bc &)
-(urxvt -e ncmpcpp -s visualizer &)
-(urxvt -T tty-clock -e sh -c "tty-clock -bc -C 1" &)
-(urxvt -T neofetch -hold -e sh -c "neofetch" &)
 (urxvt -T cal -hold -e sh -c "cal -n 2" &)
 
 # focus workspace and main window - ranger
