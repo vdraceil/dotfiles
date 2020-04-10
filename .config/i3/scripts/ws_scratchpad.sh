fa6915a56f8a65d1c6ea710435e2fd6d954c79b8
Vinoth Kumar Kannan <vdraceil@gmail.com>
2020-04-10 14:31:40 +0700

    i3wm - Adding new layout and script for scratchpad (WS10)

---
+++ .config/i3/scripts/ws_scratchpad.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+WS10="10: scratchpad"
+I3_WS_LAYOUT="$HOME/.config/i3/layouts/ws_scratchpad.json"
+
+# kill all windows in scratchpad
+i3-msg -q "[workspace=\"$WS10\"] kill"
+
+# push new layout
+i3-msg -q "workspace \"$WS10\"; append_layout $I3_WS_LAYOUT"
+
+# start applications to be absorbed into the placeholders
+(urxvt -T scratchpad &)
