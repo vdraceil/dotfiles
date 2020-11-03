0c9b2d0a8b5e7f61ce06234e2c3f785c5525fe95
Vinoth Kumar Kannan <vdraceil@gmail.com>
2020-11-03 16:42:14 +0700

    i3wm - Preloading WS4 with applications on init

---
+++ .config/i3/scripts/ws_text.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+WS4="4: text"
+I3_WS_LAYOUT="$HOME/.config/i3/layouts/ws_text.json"
+
+# kill all windows in text
+i3-msg -q "[workspace=\"$WS4\"] kill"
+
+# push new layout
+i3-msg -q "workspace \"$WS4\"; append_layout $I3_WS_LAYOUT"
+
+# start applications to be absorbed into the placeholders
+(code-oss &)
+(skypeforlinux &)
