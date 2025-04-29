981f5f11f5ce2b18e321a2447230a3605ed056df
Vinoth Kumar Kannan <vdraceil@gmail.com>
2025-04-29 12:39:59 +0700

    nvim - Updating nvim-tree options

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,6 +4,15 @@ return {
   lazy = false,
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('nvim-tree').setup {}
+    require('nvim-tree').setup({
+      git = {
+        enable = true,
+        ignore = true,
+      },
+      filters = {
+        dotfiles = false,
+        custom = { '^.git$', '.github', '.vscode', '^env$', 'node_modules', '__pycache__' },
+      },
+    })
   end,
 }
